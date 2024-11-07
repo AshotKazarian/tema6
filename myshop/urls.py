7fd10ef3cb6db0dff4bb1be0dcb3b04005d10a10
AshotKazarian <kazarian.info@gmail.com>
2024-11-07 19:19:47 +0300

    Добавлены пояснения

--- myshop/urls.py
+++ myshop/urls.py
@@ -14,3 +14,7 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
 
+# if settings.DEBUG::  Этот код выполняется только в режиме отладки (когда settings.DEBUG равно True).
+    # * urlpatterns += static(...):  Добавляет URL-шаблоны для обработки статических файлов к списку urlpatterns.
+        # * settings.MEDIA_URL:  Это URL-префикс для статических файлов (например, /media/).
+        # * settings.MEDIA_ROOT:  Это путь к папке, где хранятся статические файлы.
